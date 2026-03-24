5743cb42508eeb78ad42d46e33e1bd7cbaf75d70
umutdenizbalkan <umutdenizbalkan@gmail.com>
2026-03-24 15:05:43 +0300

    Add riscv64 and aarch64 boot asm entry glue

--- src/arch/riscv64/boot.rs
+++ src/arch/riscv64/boot.rs
@@ -1,10 +1,34 @@
-#[cfg(not(feature = "hosted-dev"))]
+#[cfg(all(not(feature = "hosted-dev"), target_arch = "riscv64"))]
+use core::arch::global_asm;
+
+#[cfg(all(not(feature = "hosted-dev"), target_arch = "riscv64"))]
+global_asm!(
+    r#"
+    .section .bss.bootstack,"aw",@nobits
+    .align 16
+boot_stack_riscv64:
+    .skip 16384
+boot_stack_riscv64_end:
+
+    .section .text.boot,"ax",@progbits
+    .globl _start
+    .type _start,@function
+_start:
+    la sp, boot_stack_riscv64_end
+    call yarm_kernel_main_riscv64
+1:
+    wfi
+    j 1b
+    "#
+);
+
+#[cfg(all(not(feature = "hosted-dev"), target_arch = "riscv64"))]
 unsafe extern "C" {
     fn yarm_kernel_main() -> !;
 }
 
-#[cfg(not(feature = "hosted-dev"))]
+#[cfg(all(not(feature = "hosted-dev"), target_arch = "riscv64"))]
 #[unsafe(no_mangle)]
-pub extern "C" fn _start() -> ! {
+pub extern "C" fn yarm_kernel_main_riscv64() -> ! {
     unsafe { yarm_kernel_main() }
 }
