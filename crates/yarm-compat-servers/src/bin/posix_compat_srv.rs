c47172745fa322e4aa33a01df225fe83ec46d6c6
umutdenizbalkan <umutdenizbalkan@gmail.com>
2026-04-24 13:44:22 +0300

    Wire startup slot install into freestanding server entrypoints

--- crates/yarm-compat-servers/src/bin/posix_compat_srv.rs
+++ crates/yarm-compat-servers/src/bin/posix_compat_srv.rs
@@ -2,7 +2,36 @@
 // Copyright 2026 Umut Deniz Balkan
 
 #![no_std]
+#![cfg_attr(not(feature = "hosted-dev"), no_main)]
 
-fn main() {
+#[inline]
+fn run() {
     yarm_server_runtime::run_posix_compat_server();
+}
+
+#[cfg(feature = "hosted-dev")]
+fn main() {
+    run();
+}
+
+#[cfg(not(feature = "hosted-dev"))]
+#[unsafe(no_mangle)]
+pub extern "C" fn _start(
+    startup_task_id: u64,
+    startup_proc_mgr_request_send_cap: u64,
+    startup_proc_mgr_reply_recv_cap: u64,
+) -> ! {
+    yarm_server_runtime::install_startup_arg_slots([
+        startup_task_id,
+        startup_proc_mgr_request_send_cap,
+        startup_proc_mgr_reply_recv_cap,
+    ]);
+    run();
+    loop {}
+}
+
+#[cfg(not(feature = "hosted-dev"))]
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo<'_>) -> ! {
+    loop {}
 }
--- crates/yarm-control-plane-servers/src/bin/vfs_server.rs
+++ crates/yarm-control-plane-servers/src/bin/vfs_server.rs
@@ -2,6 +2,7 @@
 // Copyright 2026 Umut Deniz Balkan
 
 #![cfg_attr(not(feature = "hosted-dev"), no_std)]
+#![cfg_attr(not(feature = "hosted-dev"), no_main)]
 
 #[cfg(not(feature = "hosted-dev"))]
 yarm::install_freestanding_allocator!(
@@ -9,6 +10,34 @@ yarm::install_freestanding_allocator!(
     "vfs server freestanding allocator OOM"
 );
 
-fn main() {
+#[inline]
+fn run() {
     yarm_control_plane_servers::run_vfs_server();
+}
+
+#[cfg(feature = "hosted-dev")]
+fn main() {
+    run();
+}
+
+#[cfg(not(feature = "hosted-dev"))]
+#[unsafe(no_mangle)]
+pub extern "C" fn _start(
+    startup_task_id: u64,
+    startup_proc_mgr_request_send_cap: u64,
+    startup_proc_mgr_reply_recv_cap: u64,
+) -> ! {
+    yarm_server_runtime::install_startup_arg_slots([
+        startup_task_id,
+        startup_proc_mgr_request_send_cap,
+        startup_proc_mgr_reply_recv_cap,
+    ]);
+    run();
+    loop {}
+}
+
+#[cfg(not(feature = "hosted-dev"))]
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo<'_>) -> ! {
+    loop {}
 }
