77beae83ff2b7e55889de46f4396a323ae0872fa
umutdenizbalkan <umutdenizbalkan@gmail.com>
2026-05-23 19:34:06 +0300

    test: quarantine legacy workspace integration targets

--- tests/kernel_scenarios.rs
+++ tests/kernel_scenarios.rs
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: Apache-2.0
 // Copyright 2026 Umut Deniz Balkan
 
+#![cfg(feature = "legacy-tests")]
+
 extern crate yarm;
 
 use yarm::kernel::boot::{Bootstrap, KernelError};
